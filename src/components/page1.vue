b204e2579ce83e8d24f29d99e36a9f8b6b684956
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-07-04 17:19:13 +0500

    Birinchi commit

---
+++ src/components/page1.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="p-10">
+    <div class="container relative w-full flex flex-col md:flex-row gap-5">
+      <div
+        class="left flex flex-col md:flex-row w-full md:w-1/2 items-center justify-center gap-2"
+      >
+        <div data-aos="fade-right" class="image w-full md:w-1/2">
+          <img
+            src="..//img/main.jpg"
+            alt="rasmim"
+            class="w-full object-cover h-[345px] rounded-xl"
+          />
+        </div>
+        <div
+          data-aos="fade-left"
+          class="about w-full md:w-1/2 flex flex-col justify-between h-[345px] p-5 rounded-xl bg-gray-300"
+        >
+          <h1 class="font-sans font-bold text-xl">Assalomu aleykum</h1>
+          <span>
+            <h2 class="font-bold text-xl">
+              Mirzamatov Muhsinbek Mo'ydinjon o'g'li
+            </h2>
+            <h3 class="font-normal text-gray-500">Web dasturchiman/Frontend</h3>
+          </span>
+        </div>
+      </div>
+      <div
+        data-aos="flip-left"
+        class="right w-full md:w-1/2 h-[345px] flex flex-col gap-4 bg-gray-300 rounded-xl p-5"
+      >
+        <h1 class="font-sans font-bold text-xl">Qobilyatlarim</h1>
+        <div class="skils flex flex-col gap-4 w-full">
+          <span class="w-full flex justify-between gap-3">
+            <h2 class="font-bold text-xl w-[17%]">HTML</h2>
+            <div class="w-5/6 bg-slate-200 rounded-xl">
+              <div class="w-full h-7 bg-lime-500 rounded-xl"></div>
+            </div>
+          </span>
+          <span class="w-full flex justify-between gap-3">
+            <h2 class="font-bold text-xl w-[17%]">CSS</h2>
+            <div class="w-5/6 bg-slate-200 rounded-xl">
+              <div class="w-full h-7 bg-lime-500 rounded-xl"></div>
+            </div>
+          </span>
+          <span class="w-full flex justify-between gap-3">
+            <h2 class="font-bold text-xl w-[17%]">Git</h2>
+            <div class="w-5/6 bg-slate-200 rounded-xl">
+              <div class="w-3/4 h-7 bg-lime-500 rounded-xl"></div>
+            </div>
+          </span>
+          <span class="w-full flex justify-between gap-3">
+            <h2 class="font-bold text-xl w-[17%]">JS</h2>
+            <div class="w-5/6 bg-slate-200 rounded-xl">
+              <div class="w-4/6 h-7 bg-lime-500 rounded-xl"></div>
+            </div>
+          </span>
+          <span class="w-full flex justify-between gap-3">
+            <h2 class="font-bold text-xl w-[17%]">Vue.js</h2>
+            <div class="w-5/6 bg-slate-200 rounded-xl">
+              <div class="w-1/2 h-7 bg-lime-500 rounded-xl"></div>
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script setup></script>
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Nav from "../components/Nav.vue";
+import Page1 from "../components/page1.vue";
+</script>
+
+<template>
+  <header class="sticky top-0">
+        <Nav></Nav>
+  </header>
+  <main>
+    <Page1></Page1>
+  </main>
+</template>
